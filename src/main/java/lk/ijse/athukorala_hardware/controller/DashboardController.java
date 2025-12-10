2d66a92bb5e60ba98530b0f75c7f71ae7624b223
SudoNaveenDotMe <naveenpabhath123@gmail.com>
2025-12-10 21:01:03 +0530

    Dashboard created and add some CSS

--- src/main/java/lk/ijse/athukorala_hardware/controller/DashboardController.java
+++ src/main/java/lk/ijse/athukorala_hardware/controller/DashboardController.java
@@ -1,4 +1,36 @@
 package lk.ijse.athukorala_hardware.controller;
 
-public class DashboardController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    public AnchorPane mainContent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void customerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void driverOnAction(ActionEvent actionEvent) {
+    }
+
+    public void supplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void orderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
 }
